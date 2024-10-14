aef49bee3a67b190738794d4e850071ca911ca99
JulioRiveraAguilar <161143868+JulioRiveraAguilar@users.noreply.github.com>
2024-10-13 19:07:08 -0600

    Segundo ejercicio de la guia1
    Se agrega el segundo ejercicio terminado

---
+++ Guia1/Ejercicio2.sql
@@ -0,0 +1,20 @@
+/**
+	Ejercicio 2: Funcion Escalar para Convertir Texto a Minusculas
+	Crea una funcion escalar que reciba un texto en mayusculas o mixto y devuelva el
+	texto en minusculas.
+	Parametro de entrada: Un texto (NVARCHAR).
+	Salida: El texto convertido en minusculas (NVARCHAR).
+**/
+CREATE OR ALTER FUNCTION dbo.funcionConvertirTextoMayuscula(
+	@nombresEmpleado NVARCHAR(100)
+)
+RETURNS NVARCHAR(100)
+AS
+	BEGIN
+		DECLARE @nombresMayus NVARCHAR(100)
+		SET @nombresMayus = LOWER(@nombresEmpleado)
+		RETURN @nombresMayus
+	END
+GO
+SELECT dbo.funcionConvertirTextoMayuscula('Laura') AS Nombre_Minuscula
+GO
